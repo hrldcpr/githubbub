dce9dd3b2f4a2df51672c8705b52ca3313dd2e58
Harold Cooper <hrldcpr@gmail.com>
2012-04-08 17:59:39 -0400

    better rounded borders, and no titles

--- githubbub.js
+++ githubbub.js
@@ -20,7 +20,7 @@ var textPaths = {
 function makeDiv(event, text) {
     var x = Math.floor(80 * Math.random());
     var y = Math.floor(80 * Math.random());
-    return $('<a href="https://github.com/' + event.actor.login + '" title="' + event.actor.login + '"' +
+    return $('<a href="https://github.com/' + event.actor.login + '"' +
 	     ' class="bubble" style="top:' + x + '%; left: ' + y + '%;">' +
 	     '<img src="http://gravatar.com/avatar/' + event.actor.gravatar_id + '?d=retro"/>' +
 	     '<div class="text">' + text + '</div></a>');
