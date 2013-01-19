e92a113005a8c5e5a8ec0d036b6328e7ea9471ce
Harold Cooper <hrldcpr@gmail.com>
2013-01-19 01:51:25 -0500

    use github api proxy to avoid rate-limits

--- githubbub.js
+++ githubbub.js
@@ -48,7 +48,8 @@ function makeDivs(event) {
 }
 
 function update() {
-    $.get("https://api.github.com/events", {}, function(events) {
+    // TODO only start using local proxy after client's direct api connection has been rate-limited
+    $.get("/events", {}, function(events) {
 	var divs = $.map(events.data.reverse(), function(event) {
 	    if (event.id > maxId) {
 		maxId = event.id;
