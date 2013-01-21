3e93876d5a5f78578a214b1fc9d9863d7e1909f3
Harold Cooper <hrldcpr@gmail.com>
2013-01-21 15:29:40 -0500

    'custom' urls for more events

--- githubbub.js
+++ githubbub.js
@@ -5,6 +5,14 @@ var maxId = 0;
 var allDivs = [];
 var paused = false;
 
+function repoUrl(event) {
+    return 'https://github.com/' + event.repo.name;
+}
+
+function userUrl(event) {
+    return 'https://github.com/' + event.actor.login;
+}
+
 var textPaths = {
     CommitCommentEvent: ['comment', 'body'],
     CreateEvent: ['description'],
@@ -18,9 +26,22 @@ var textPaths = {
     PullRequestReviewCommentEvent: ['comment', 'body'],
 };
 
-function makeDiv(event, text, url) {
-    if (!url) url = 'https://github.com/' + event.actor.login;
+var urlPaths = {
+    CommitCommentEvent: ['comment', 'html_url'],
+    ForkEvent: ['forkee', 'html_url'],
+    IssuesEvent: ['issue', 'html_url'],
+    PullRequestEvent: ['pull_request', 'html_url'],
+    PullRequestReviewCommentEvent: ['comment', '_links', 'html'],
+};
 
+var urlGetters = {
+    CreateEvent: repoUrl,
+    IssueCommentEvent: function(event) {
+        return event.payload.issue.html_url + '#issuecomment-' + event.payload.comment.id;
+    },
+}
+
+function makeDiv(event, text, url) {
     var x = Math.floor(80 * Math.random());
     var y = Math.floor(80 * Math.random());
     var bubble = $('<a href="' + url + '" class="bubble git" style="top:' + x + '%; left: ' + y + '%;">' +
@@ -30,28 +51,39 @@ function makeDiv(event, text, url) {
     return bubble;
 }
 
+function walk(x, path) {
+    if (x && path) {
+        for (var i in path) {
+	    x = x[path[i]];
+	    if (!x) return;
+        }
+        return x;
+    }
+}
+
 function makeDivs(event) {
     if (event.type === 'PushEvent') { // contains multiple texts
 	return $.map(event.payload.commits, function(commit) {
-            var url = "https://github.com/" + event.repo.name + "/commit/" + commit.sha;
+            var url = repoUrl(event) + '/commit/' + commit.sha;
 	    return makeDiv(event, commit.message, url);
 	});
     }
-    var path = textPaths[event.type];
-    if (path) {
-	var text = event.payload;
-	for (var i in path) {
-	    text = text[path[i]];
-	    if (!text) return;
-	}
-	return [makeDiv(event, text)];
+
+    var text = walk(event.payload, textPaths[event.type]);
+    if (text) {
+        // first try urlPaths, then urlGetters, then default to user url:
+        var url = walk(event.payload, urlPaths[event.type]);
+        if (!url) url = (urlGetters[event.type] || userUrl)(event);
+
+	return [makeDiv(event, text, url)];
     }
+
     //console.log(event); // skipped event
 }
 
 var direct = true; // initially call github api directly
 function update() {
-    $.get(direct ? "https://api.github.com/events" : "/events", {}, function(events) {
+    $.get(direct ? 'https://api.github.com/events' : '/events', {}, function(events) {
         if (direct) events = events.data; // when using jsonp, github api nests its response in 'data'
 
         if (Object.prototype.toString.call(events) !== '[object Array]') {
