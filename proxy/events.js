3a47e37dcb7f0b10933ce77875e742fa4641c7bc
Harold Cooper <hrldcpr@gmail.com>
2013-01-21 13:57:52 -0500

    log multiple listeners

--- proxy/events.js
+++ proxy/events.js
@@ -34,7 +34,7 @@ function pipeGithubEvents(out) {
                     listeners = [];
                 });
             });
-        }
+        } else console.log(' ' + listeners.length + ' listeners');
     }
 }
 
